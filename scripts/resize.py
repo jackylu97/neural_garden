f3a64be054f5d89461a6a3e10bb1c24113885bc7
Jacky Lu <jackylu97@gmail.com>
2020-03-16 18:34:18 -0700

    add resize script

---
+++ scripts/resize.py
@@ -0,0 +1,11 @@
+import Augmentor
+
+# Augmentor doesn't reset working directory after
+# navigating to INPUT_PATH, apparently
+INPUT_PATH = "./gallery-dl/output"
+OUTPUT_PATH = "../../resized"
+DIM = 512
+
+p = Augmentor.Pipeline(INPUT_PATH, OUTPUT_PATH)
+p.resize(probability=1.0, width=DIM, height=DIM)
+p.process()
