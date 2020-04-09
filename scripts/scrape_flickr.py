cd75bbc5934787169e48893d0e212bf7dbc61066
Jacky Lu <jackylu97@gmail.com>
2020-04-08 17:40:34 -0700

    scrape script bugfix

--- scripts/scrape_flickr.py
+++ scripts/scrape_flickr.py
@@ -49,13 +49,21 @@ text_file.close()
 
 # run gallery-dl to scrape flickr
 flickr_groups = [
-    "https://www.flickr.com/groups/979936@N25/"
-    "https://www.flickr.com/groups/2297498@N20/"
-    "https://www.flickr.com/groups/1003995@N21/"
-    "https://www.flickr.com/groups/375216@N23/"
+    "https://www.flickr.com/groups/979936@N25/",
+    "https://www.flickr.com/groups/2297498@N20/",
+    "https://www.flickr.com/groups/1003995@N21/",
+    "https://www.flickr.com/groups/375216@N23/",
 ]
 
-scrapeCommand = "gallery-dl --range 1-10 -c gallery-dl.conf https://www.flickr.com/groups/2297498@N20/"
-process = subprocess.Popen(scrapeCommand.split(), stdout=subprocess.PIPE)
-for line in iter(process.stdout.readline, b""):
-    sys.stdout.write(line.decode("utf-8"))
+ct = 0
+for url in flickr_groups:
+    scrapeCommand = "gallery-dl --range 1-5000 -c gallery-dl.conf {}".format(
+        url)
+    process = subprocess.Popen(scrapeCommand.split(), stdout=subprocess.PIPE)
+    for line in iter(process.stdout.readline, b""):
+        sys.stdout.write(line.decode("utf-8"))
+        ct += 1
+        if ct % 200 == 0:
+            print("Downloaded {} images thus far.".format(ct))
+    print("{}\n has completed with error code: {}".format(
+        scrapeCommand, process.wait()))
